979f1550937e51061757a3ca46b8031d612f24ee
Steven Maloney <steven.maloney@gmail.com>
2021-01-11 22:25:39 -0600

    dropdown menu for play selector contains all plays

--- src/components/CreatePlay/CreatePlay.js
+++ src/components/CreatePlay/CreatePlay.js
@@ -1,14 +1,16 @@
 import React, {useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 function CreatePlay() {
     const dispatch = useDispatch();
+    const playList = useSelector(state=> state.allPlays);
     useEffect(() => {dispatch({type: 'FETCH_ALL_PLAYS'})}, []);
     return(
         <div>
             <label>Select Play from the List below</label>
             <select>
-
+                <option>Choose a Subject</option>
+                {playList.map((p) => (<option key={p.id} value={p.play_code}>{p.play_name}</option>))}
             </select>
         </div>
     )
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import allPlays from './user.reducer';
+import allPlays from './allPlays.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
